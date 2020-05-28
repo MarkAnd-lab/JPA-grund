68e6a76e10ed7de69896810bc94ccf73d5a581ba
MarkAnd-lab <mark-andersson@hotmail.com>
2020-05-28 19:39:20 +0200

    Test

---
+++ src/test/java/se/lexicon/mark/JPAgrund/DaoTest.java
@@ -0,0 +1,24 @@
+package se.lexicon.mark.JPAgrund;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DaoTest {
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void merge() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
