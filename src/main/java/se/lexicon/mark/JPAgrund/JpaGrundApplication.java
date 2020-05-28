11d58c27d8978c95aa3ec114f52d9a79f0490662
MarkAnd-lab <mark-andersson@hotmail.com>
2020-05-28 09:18:33 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/mark/JPAgrund/JpaGrundApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.mark.JPAgrund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaGrundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaGrundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/mark/JPAgrund/JpaGrundApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.mark.JPAgrund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaGrundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
