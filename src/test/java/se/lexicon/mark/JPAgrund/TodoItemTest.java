09554cb36150595f9723aa0c254d11924392c10c
MarkAnd-lab <mark-andersson@hotmail.com>
2020-05-29 20:34:36 +0200

    TodoItemTest

---
+++ src/test/java/se/lexicon/mark/JPAgrund/TodoItemTest.java
@@ -0,0 +1,44 @@
+package se.lexicon.mark.JPAgrund;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodoItemTest {
+
+    @Test
+    void getTodoId() {
+    }
+
+    @Test
+    void getTitle() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getDeadline() {
+    }
+
+    @Test
+    void isDone() {
+    }
+
+    @Test
+    void toggleDone() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
